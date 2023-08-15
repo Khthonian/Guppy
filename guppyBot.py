a2d15a19e6e1b0fd05f7cf07885f8e70214ab456
Khthonian <anansicorp@tuta.io>
2023-08-15 20:26:39 +0100

    Update to work with Replit

--- guppyBot.py
+++ guppyBot.py
@@ -1,6 +1,9 @@
 import discord
 import json
+import os
 from discord.ext import commands
+
+myKey = os.environ['TOKEN']
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,34 +13,42 @@ bot = commands.Bot(command_prefix='/guppy ', intents=intents)
 
 # Load Git command data from the JSON
 with open('commands.json', 'r') as file:
-    gitCommands = json.load(file)
+  gitCommands = json.load(file)
+
 
 # Log in with the bot
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+  print(f"We have logged in as {bot.user}")
+
 
 # Define a command to output a list of commands
 @bot.command(name='all')
 async def guppyAll(ctx):
-    # Create a list of all the commands
-    allCommands = "\n" + "# Available Commands\n- " + "\n- ".join(gitCommands.keys()) + "\n"
-    await ctx.send(allCommands)
+  # Create a list of all the commands
+  allCommands = "\n" + "# Available Commands\n- " + "\n- ".join(
+    gitCommands.keys()) + "\n"
+  await ctx.send(allCommands)
+
 
 # Define a command to output specific commands
 @bot.command(name='command')
 async def guppyCommand(ctx, *, command=None):
-    # Check if the user gave a command
-    if command is None:
-        await ctx.send("Please provide a Git command for help. For example: `/guppy command git init`")
-        return
+  # Check if the user gave a command
+  if command is None:
+    await ctx.send(
+      "Please provide a Git command for help. For example: `/guppy command git init`"
+    )
+    return
 
-    # Output the description for the user-defined command
-    description = gitCommands.get(command)
-    if description:
-        await ctx.send(description)
-    else:
-        await ctx.send(f"Sorry, I don't have information on the command `{command}`. Please check the command and try again.")
+  # Output the description for the user-defined command
+  description = gitCommands.get(command)
+  if description:
+    await ctx.send(description)
+  else:
+    await ctx.send(
+      f"Sorry, I don't have information on the command `{command}`. Please check the command and try again."
+    )
 
 # Run the bot
-bot.run('TOKEN')
+bot.run(myKey)
